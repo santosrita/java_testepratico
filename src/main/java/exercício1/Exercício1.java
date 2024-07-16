4883fa06bcec37066adb0653fdcbd2063f38c1ac
Bowie <santoscsrita@gmail.com>
2024-07-16 16:05:52 -0300

    Abertura de scanner para ler 4 notas

--- src/main/java/exercício1/Exercício1.java
+++ src/main/java/exercício1/Exercício1.java
@@ -1,10 +1,26 @@
 package exercício1;
 
+import java.util.Scanner;
+
 public class Exercício1 {
     public static void main(String[] args) {
 
 
         double n1, n2, n3, n4;
-        double md; 
+        double md;
+        Scanner input = new Scanner(System.in)
+
+        System.out.println("Informe a primeira nota:");
+        n1 = input.nextDouble();
+
+        System.out.println("Informe a segunda nota:");
+        n2 = input.nextDouble();
+
+        System.out.println("Informe a terceira nota:");
+        n3 = input.nextDouble();
+
+        System.out.println("Informe a quarta nota:");
+        n4 = input.nextDouble();
+
     }
 }
