266d8f8c5422c45d8161889dddba251e78fc7e7b
Bowie <santoscsrita@gmail.com>
2024-07-17 13:21:40 -0300

    Calculando o IMC

--- src/main/java/exercício6/Exercicio6.java
+++ src/main/java/exercício6/Exercicio6.java
@@ -16,13 +16,23 @@ public class Exercicio6 {
 
         //Solicitando o sexo ao usuário
         System.out.println("Digite o sexo (M para masculino e F para feminino)");
-        sexo = input.nextDouble();
+        sexo = input.next();
 
-        
-
-
-
-
+        //Calculando o IMC conforme o sexo
+        if (sexo.equalsIgnoreCase("M")){
+            //Fórmula para homens
+            imcIdeal = (72.7 * altura) - 58;
+            System.out.println("O IMC ideal para um homem com altura " + altura + "é: " + imcIdeal);
+        }
+        else if (sexo.equalsIgnoreCase("F")){
+            //Fórmula para mulheres
+            imcIdeal = (62.1 * altura) - 44.7;
+            System.out.println("O IMC ideal para uma mulher com altura " + altura + "é: " + imcIdeal);
+        }
+        else {
+            //Se o sexo não for válido
+            System.out.println("Sexo inválido. Por favor, digite M para masculino ou F para feminino.");
+        }
 
         input.close();
 
