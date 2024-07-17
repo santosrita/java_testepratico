a12c203561cc2a81646300e8fd55a5fec7247990
Bowie <santoscsrita@gmail.com>
2024-07-17 10:18:24 -0300

    Solicitando os valores ao usuário

--- src/main/java/exercício3/Exercício3.java
+++ src/main/java/exercício3/Exercício3.java
@@ -8,6 +8,13 @@ public class Exercício3 {
         int A, B;
         Scanner input = new Scanner(System.in)
 
+                //Solicitando os valores ao usuário
+        System.out.println("Digite o valor de A");
+        A = input.nextInt();
+
+        System.out.println("Digite o valor de B");
+        B = input.nextInt();
+
 
     }
 }
