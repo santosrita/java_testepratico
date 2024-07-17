b3fc5da85a611966c0e0a2b6d17258f36161e1a9
Bowie <santoscsrita@gmail.com>
2024-07-17 10:39:56 -0300

    Solicitando um valor ao usuário

--- src/main/java/exercício4/Exercício4.java
+++ src/main/java/exercício4/Exercício4.java
@@ -6,7 +6,11 @@ public class Exercício4 {
     public static void main(String[] args) {
 
         int numero
-                Scanner input = new Scanner(System.in)
+                Scanner input = new Scanner(System.in);
+
+        //Solicitando valor ao usuário
+        System.out.println("Digite um valor");
+        numero = input.nextInt();
 
 
 
