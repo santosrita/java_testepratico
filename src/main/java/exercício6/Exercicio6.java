748ad7fc62b5f64d0019571be068c49b6ae5e1b5
Bowie <santoscsrita@gmail.com>
2024-07-17 13:06:58 -0300

    Iniciando um novo scanner

--- src/main/java/exercício6/Exercicio6.java
+++ src/main/java/exercício6/Exercicio6.java
@@ -1,11 +1,16 @@
 package exercício6;
 
+import java.util.Scanner;
+
 public class Exercicio6 {
     public static void main(String[] args) {
 
         String sexo;
         Double altura;
         Double imcIdeal;
+        Scanner input = new Scanner(System.in);
+        
+        input.close();
 
     }
 }
