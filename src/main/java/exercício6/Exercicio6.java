5b7080700b29445f4f7fe54a7d3dda0cd2323186
Rita Santos <134738657+santosrita@users.noreply.github.com>
2024-07-17 13:25:31 -0300

    Master (#6)
    * Criação da classe Exercicio6
    
    * Criação das variáveis sexo, altura e imcIdeal
    
    * Iniciando um novo scanner
    
    * Solicitando a altura ao usuário
    
    * Solicitando o sexo ao usuário
    
    * Calculando o IMC
    
    * Finalizando o programa

---
+++ src/main/java/exercício6/Exercicio6.java
@@ -0,0 +1,40 @@
+package exercício6;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+
+        String sexo;
+        Double altura;
+        Double imcIdeal;
+        Scanner input = new Scanner(System.in);
+
+        //Solicitando a altura ao usuário
+        System.out.println("Digite a altura em metros");
+        altura = input.nextDouble();
+
+        //Solicitando o sexo ao usuário
+        System.out.println("Digite o sexo (M para masculino e F para feminino)");
+        sexo = input.next();
+
+        //Calculando o IMC conforme o sexo
+        if (sexo.equalsIgnoreCase("M")){
+            //Fórmula para homens
+            imcIdeal = (72.7 * altura) - 58;
+            System.out.println("O IMC ideal para um homem com altura " + altura + "m é: " + imcIdeal);
+        }
+        else if (sexo.equalsIgnoreCase("F")){
+            //Fórmula para mulheres
+            imcIdeal = (62.1 * altura) - 44.7;
+            System.out.println("O IMC ideal para uma mulher com altura " + altura + "m é: " + imcIdeal);
+        }
+        else {
+            //Se o sexo não for válido
+            System.out.println("Sexo inválido. Por favor, digite M para masculino ou F para feminino.");
+        }
+
+        input.close();
+
+    }
+}
