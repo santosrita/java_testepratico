72b87873d73658efbabc4149e3306e55874d26bb
Bowie <santoscsrita@gmail.com>
2024-07-17 13:34:10 -0300

    Iterando de 10 até 1 usando um loop for

--- src/main/java/exercício7/Exercicio7.java
+++ src/main/java/exercício7/Exercicio7.java
@@ -1,4 +1,12 @@
 package exercício7;
 
 public class Exercicio7 {
+    public static void main(String[] args) {
+
+        int i;
+
+        // Iterando de 10 até 1 usando um loop for
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+    }
 }
