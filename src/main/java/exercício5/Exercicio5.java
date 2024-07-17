f6c1ba856e6b20bd7538e6a504f4328a30512e52
Bowie <santoscsrita@gmail.com>
2024-07-17 11:39:33 -0300

    Solicitando valores ao usuário

--- src/main/java/exercício5/Exercicio5.java
+++ src/main/java/exercício5/Exercicio5.java
@@ -6,6 +6,13 @@ public class Exercicio5 {
     public static void main(String[] args) {
 
         int numero1, numero2;
-        Scanner input = new Scanner(System.in)
+        Scanner input = new Scanner(System.in);
+
+        //Solicitando os valores ao usuário
+        System.out.println("Digite o primeiro número");
+        numero1 = input.nextInt();
+
+        System.out.println("Digite o segundo número");
+        numero2 = input.nextInt();
     }
 }
