e8ecce8fdb2172bf8c0cf0b4f53cea8225579e98
Bowie <santoscsrita@gmail.com>
2024-07-19 14:05:30 -0300

    Aplicação do loop

--- src/main/java/exercício10/Exercicio10.java
+++ src/main/java/exercício10/Exercicio10.java
@@ -4,5 +4,11 @@ public class Exercicio10 {
     public static void main(String[] args) {
 
         int numero = 7;
+
+        // Loop de 1 a 10 para calcular e exibir a tabuada
+        for (int i = 1; i <= 10; i++) {
+            int resultado = numero * i;
+            System.out.println(numero + " X " + i + " = " + resultado);
+        }
     }
 }
