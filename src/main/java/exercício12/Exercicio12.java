7a22d3801c6b6f3fec6265032aa47aab6e0c0bb1
Bowie <santoscsrita@gmail.com>
2024-07-19 14:32:56 -0300

    Finalização do programa 12

--- src/main/java/exercício12/Exercicio12.java
+++ src/main/java/exercício12/Exercicio12.java
@@ -1,2 +1,50 @@
-package exercício12;public class Exercicio12 {
+package exercício12;
+
+import java.util.Scanner;
+
+public class Exercicio12 {
+    public static void main(String[] args) {
+
+        int numero;
+        Scanner input = new Scanner(System.in);
+
+        // Solicita ao usuário que digite um número de 1 a 7
+        System.out.print("Digite um número de 1 a 7 para obter o dia da semana: ");
+        numero = input.nextInt();
+
+        // Usa switch-case para determinar o dia da semana correspondente
+        String diaDaSemana;
+        switch (numero) {
+            case 1:
+                diaDaSemana = "Domingo";
+                break;
+            case 2:
+                diaDaSemana = "Segunda-feira";
+                break;
+            case 3:
+                diaDaSemana = "Terça-feira";
+                break;
+            case 4:
+                diaDaSemana = "Quarta-feira";
+                break;
+            case 5:
+                diaDaSemana = "Quinta-feira";
+                break;
+            case 6:
+                diaDaSemana = "Sexta-feira";
+                break;
+            case 7:
+                diaDaSemana = "Sábado";
+                break;
+            default:
+                diaDaSemana = "Número inválido. Digite um número de 1 a 7.";
+                break;
+        }
+
+        // Exibe o dia da semana correspondente ou mensagem de erro
+        System.out.println(diaDaSemana);
+
+        // Fecha o Scanner
+        input.close();
+    }
 }
