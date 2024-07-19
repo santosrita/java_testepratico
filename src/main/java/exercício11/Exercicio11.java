893dff8ef4bcc56f442af6dfb3e10a3e03b63147
Bowie <santoscsrita@gmail.com>
2024-07-19 14:13:14 -0300

    Abertura de um novo scanner

--- src/main/java/exercício11/Exercicio11.java
+++ src/main/java/exercício11/Exercicio11.java
@@ -1,8 +1,11 @@
 package exercício11;
 
+import java.util.Scanner;
+
 public class Exercicio11 {
     public static void main(String[] args) {
 
         int numero;
+        Scanner input = new Scanner(System.in)
     }
 }
